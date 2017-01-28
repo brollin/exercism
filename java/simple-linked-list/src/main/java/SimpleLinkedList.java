fd02ca1e230bd6f01c4701233520785160a5db16
brollin <rollin.ben@gmail.com>
2017-01-27 18:30:51 -0800

    Adding exercism java simple-linked-list exercise with working test.

---
+++ java/simple-linked-list/src/main/java/SimpleLinkedList.java
@@ -0,0 +1,34 @@
+
+
+public class SimpleLinkedList {
+    
+    private int size = 0;
+
+    public SimpleLinkedList() {
+
+    }
+    
+    public SimpleLinkedList(Object[] objects) {
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object pop() {
+        return new Object();
+    }
+
+    public void push(Object obj) {
+
+    }
+
+    public void reverse() {
+
+    }
+
+    public Object[] asArray(Class class_) {
+        return new Object[2];
+    }
+}
